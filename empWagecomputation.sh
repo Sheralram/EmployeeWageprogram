ac2da504857e6a8863981ec2e26522307dd309e0
Sheralram <ramkrishna.sheral@gmail.com>
2021-07-25 17:39:16 +0530

    Wages for a 20 Working Days per Month

--- empWagecomputation.sh
+++ empWagecomputation.sh
@@ -6,33 +6,36 @@ isPresentFulltime=1
 isParttime=2
 isabsent=0
 Wageperhour=20
+WorkingDayperMonth=20 
 Random=$((RANDOM%3))
 echo $Random
    if [ $Random -eq 1 ]
 then
 	echo "Employee is Full time Present "
-	EmployeeWage=$((8*20))
+	EmployeeWage=$((8*20*20))
 	echo "The Full time Employee wage is" $EmployeeWage
   elif [ $Random -eq 2 ]
 then
 	echo "Employee is Parttime Present"
-	EmployeeWage=$((8*20))
-	echo "The Part time Employee Wage is" $EmployeeWage
+	EmployeeWage=$((8*20*20))
+	echo "The Parttime Employee Wage is" $EmployeeWage
 else
 	echo "Employee is absent "
-	echo "Salary is Zero"
+	echo "EmployeeWage is Zero"
  fi 
 
 case $Random in
 
 	1) echo "Employee is Full time Present "
-		EmployeeWage=$((8*20))
+		EmployeeWage=$((8*20*20))
 		echo "Employee Full time Present wage is"$EmployeeWage
 			;;
          2) echo "Employee is Parttime Present "
-                EmployeeWage=$((8*20))
+                EmployeeWage=$((8*20*20))
                 echo "Employee Part time Present wage is"$EmployeeWage
 				;;
 	*) echo "Employee is absent"
 		echo "Salary is Zero"
 esac
+
+
